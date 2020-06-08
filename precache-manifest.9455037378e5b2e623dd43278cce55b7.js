88586c01e5a676f87d2f139916bf02e3e9852420
Lars Rasmussen <lars@lars-rasmussen.de>
2020-06-08 15:09:43 +0200

    Updates

---
+++ precache-manifest.9455037378e5b2e623dd43278cce55b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80443a1e74920260c15012a1cb3e877d",
+    "url": "/behindtheapple/index.html"
+  },
+  {
+    "revision": "1fbd215952b13496cbcf",
+    "url": "/behindtheapple/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1a7ef94e2c115198cce",
+    "url": "/behindtheapple/static/js/2.0c57156e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/behindtheapple/static/js/2.0c57156e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fbd215952b13496cbcf",
+    "url": "/behindtheapple/static/js/main.b1a3ff3c.chunk.js"
+  },
+  {
+    "revision": "c00553e53d44868d5ff5",
+    "url": "/behindtheapple/static/js/runtime-main.3c82ce7b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/behindtheapple/static/media/logo.5d5d9eef.svg"
+  }
+]);
